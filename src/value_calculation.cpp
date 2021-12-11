8916eb2223f58e10afaa496b3f67e0280bd66141
Thunraz <thunraz@thunraz.com>
2021-12-11 17:17:16 +0100

    Add boss monster test part one

--- src/value_calculation.cpp
+++ src/value_calculation.cpp
@@ -1,6 +1,20 @@
 #include "value_calculation.hpp"
+#include <set>
 
 int calculateValueAfterFirstLoop(std::vector<std::shared_ptr<Command>> const& commands)
 {
-    return 5;
+    RunInfo state { 0, 0 };
+    std::set<int> visitedLines;
+
+    while (true) {
+        visitedLines.insert(state.line);
+        auto currentCommand = commands.at(state.line);
+        state = currentCommand->execute(state);
+
+        if (visitedLines.count(state.line)) {
+            break;
+        }
+    }
+
+    return state.value;
 }
--- tests/boss_monster_test.cpp
+++ tests/boss_monster_test.cpp
@@ -1,8 +1,9 @@
 #include "parser.hpp"
 #include "value_calculation.hpp"
 #include <gtest/gtest.h>
+#include "boss_monster_input.hpp"
 
-TEST(BossMonsterTest, BossMonsterPartOne)
+TEST(BossMonsterTest, BossMonsterPreparation)
 {
     auto input = R"(nop +0
 acc +1
@@ -15,6 +16,14 @@ jmp -4
 acc +6)";
 
     auto commands = parseCommands(input);
+    auto expectedValue = 5;
+
+    ASSERT_EQ(calculateValueAfterFirstLoop(commands), expectedValue);
+}
+
+TEST(BossMonsterTest, BossMonsterTestOne)
+{
+    auto commands = parseCommands(bossMonsterInput);
     auto expectedValue = 5;
 
     ASSERT_EQ(calculateValueAfterFirstLoop(commands), expectedValue);
